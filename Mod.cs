17b0dedef737abb84e22a7699ccf9a6fb58c6f0f
ohayo <9443476+ohayo@users.noreply.github.com>
2026-05-17 15:52:13 +1000

    Initial code

---
+++ Mod.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadeModManager
+{
+    public class Mod
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Version { get; set; }
+
+        public bool Installed { get; set; } = false;
+    }
+}
---
+++ ModManagerSettings.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Windows.Documents;
+
+namespace FacadeModManager
+{
+    public class ModManagerSettings
+    {
+        public string FacadePath { get; set; }
+
+        public List<Mod> InstalledMods = new List<Mod>();
+    }
+}
